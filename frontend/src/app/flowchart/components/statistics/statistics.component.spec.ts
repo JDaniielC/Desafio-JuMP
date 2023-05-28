ce76dc24dc161f3d513f737560f8d884130eca25
José Daniel <jdsc2@cin.ufpe.br>
2023-05-28 09:58:38 -0300

    TEST: Analysis, Statistics and fix Node

--- frontend/src/app/flowchart/components/statistics/statistics.component.spec.ts
+++ frontend/src/app/flowchart/components/statistics/statistics.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 import { StatisticsComponent } from './statistics.component';
+import { DurationTimeBySecondsPipe } from 'src/app/shared/pipes/duration-time-by-seconds.pipe';
 
 describe('StatisticsComponent', () => {
   let component: StatisticsComponent;
@@ -8,10 +10,13 @@ describe('StatisticsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ StatisticsComponent ]
+      declarations: [ StatisticsComponent, DurationTimeBySecondsPipe ],
+      imports: [ MatCardModule, MatDividerModule ]
     })
     .compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(StatisticsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
